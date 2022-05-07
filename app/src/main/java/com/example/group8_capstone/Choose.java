a0c6b3121493fcb04d79f0009cf63ed4000a82b6
Jaen Toyoda <therubikators@gmail.com>
2022-05-07 13:31:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/group8_capstone/Choose.java
@@ -0,0 +1,23 @@
+package com.example.group8_capstone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+public class Choose extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.choose);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+    }
+}
---
+++ app/src/main/java/com/example/group8_capstone/Login.java
@@ -0,0 +1,22 @@
+package com.example.group8_capstone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+public class Login extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.login);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+    }
+}
